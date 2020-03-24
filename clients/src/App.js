1a34732852a735df02fb719c12f68b4cde74eda0
unknown <mehedi5051@gmail.com>
2020-03-24 18:52:29 +0600

    added react as a frontend

---
+++ clients/src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+
+function App() {
+	return (
+		<BrowserRouter>
+			<div className="container">
+				<Switch>
+					<Route exact path="/" component={Home} />
+					<Route path="/login" component={Login} />
+					<Route path="/register" component={Register} />
+				</Switch>
+			</div>
+		</BrowserRouter>
+	);
+}
+
+export default App;
