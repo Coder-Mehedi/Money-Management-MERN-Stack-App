73f193d5aafd2c2e03cfdd05ddbaae5acfbe2a42
unknown <mehedi5051@gmail.com>
2020-03-24 20:04:05 +0600

    login register page modifyed and updated

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,7 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-	return <div>Login</div>;
+	const [loginInfo, setLoginInfo] = useState({
+		email: "",
+		password: "",
+		error: {}
+	});
+	const { email, password, error } = loginInfo;
+	const handleChange = e => {
+		setLoginInfo({
+			...loginInfo,
+			[e.target.name]: e.target.value
+		});
+	};
+
+	const handleSubmit = e => {
+		e.preventDefault();
+	};
+	return (
+		<div className="row">
+			<div className="col-md6 offset-md-3">
+				<h1 className="text-center display-4">Login Here</h1>
+				<form onSubmit={handleSubmit}>
+					<div className="form-group">
+						<label htmlFor="email">email</label>
+						<input
+							type="text"
+							className="form-control"
+							placeholder="Enter Your Email"
+							email="email"
+							id="email"
+							name="email"
+							value={email}
+							onChange={handleChange}
+						/>
+					</div>
+
+					<div className="form-group">
+						<label htmlFor="password">Password</label>
+						<input
+							type="text"
+							className="form-control"
+							placeholder="Enter Your Password"
+							name="password"
+							id="password"
+							onChange={handleChange}
+							value={password}
+						/>
+					</div>
+					<Link to="/register">Don't Have Account? Register Here</Link>
+					<button className="btn btn-primary d-block my-3">Login</button>
+				</form>
+			</div>
+		</div>
+	);
 };
 
 export default Login;
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,7 +1,88 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Register = () => {
-	return <div>Register</div>;
+	const [registerInfo, setRegisterInfo] = useState({
+		name: "",
+		email: "",
+		password: "",
+		confirmPassword: "",
+		error: {}
+	});
+	const { name, email, password, confirmPassword, error } = registerInfo;
+	const handleChange = e => {
+		setRegisterInfo({
+			...registerInfo,
+			[e.target.name]: e.target.value
+		});
+	};
+
+	const handleSubmit = e => {
+		e.preventDefault();
+	};
+	return (
+		<div className="row">
+			<div className="col-md6 offset-md-3">
+				<h1 className="text-center display-4">Register Here</h1>
+				<form onSubmit={handleSubmit}>
+					<div className="form-group">
+						<label htmlFor="name">Name</label>
+						<input
+							type="text"
+							className="form-control"
+							placeholder="Enter Your Name"
+							name="name"
+							id="name"
+							value={name}
+							onChange={handleChange}
+						/>
+					</div>
+
+					<div className="form-group">
+						<label htmlFor="email">email</label>
+						<input
+							type="text"
+							className="form-control"
+							placeholder="Enter Your Email"
+							email="email"
+							id="email"
+							name="email"
+							value={email}
+							onChange={handleChange}
+						/>
+					</div>
+
+					<div className="form-group">
+						<label htmlFor="password">Password</label>
+						<input
+							type="text"
+							className="form-control"
+							placeholder="Enter Your Password"
+							name="password"
+							id="password"
+							onChange={handleChange}
+							value={password}
+						/>
+					</div>
+
+					<div className="form-group">
+						<label htmlFor="confirmPassword">Confirm Password</label>
+						<input
+							type="text"
+							className="form-control"
+							placeholder="Enter Your Confirm Password"
+							name="confirmPassword"
+							id="confirmPassword"
+							onChange={handleChange}
+							value={confirmPassword}
+						/>
+					</div>
+					<Link to="/login">Already Have An Account? Login Here</Link>
+					<button className="btn btn-primary d-block my-3">Register</button>
+				</form>
+			</div>
+		</div>
+	);
 };
 
 export default Register;
